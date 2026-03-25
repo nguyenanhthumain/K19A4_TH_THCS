2edc6a416957b72e95f3cd97e3e6e7abbe44eb82
LeDucLoc-25174600220-19A4 <ldloc.25174600220@sv.uneti.edu.vn>
2026-03-25 17:09:02 +0700

    Delete 4.1_25174600066_LuongPhuLUU directory

--- 4.1_25174600066_LuongPhuLUU/Bài_3.py
+++
@@ -1,9 +0,0 @@
-def nhan_an_do(a, b):
-    ket_qua = 0
-    while b > 0:
-        if b % 2 == 1:
-            ket_qua += a
-        a *= 2
-        b //= 2
-    return ket_qua
-print(nhan_an_do(5, 6))
