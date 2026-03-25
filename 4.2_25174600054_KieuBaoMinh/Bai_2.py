74059a2b3e762b65d5655a334cfe72ebdcbcbac0
Kiều Bảo Minh-25174600054-DHKL19A4HN <kbminh.25174600054@sv.uneti.edu.vn>
2026-03-25 17:13:33 +0700

    Delete 4.2_25174600054_KieuBaoMinh directory

--- 4.2_25174600054_KieuBaoMinh/Bai_2.py
+++
@@ -1,8 +0,0 @@
-while True:
-    tu = int(input("Nhập tử số: "))
-    mau = int(input("Nhập mẫu số: ")) 
-    if mau != 0:
-        break
-    print("Mẫu phải khác 0, nhập lại!")
-print("Phân số là:", tu, "/", mau)
-print("Giá trị =", tu / mau)
