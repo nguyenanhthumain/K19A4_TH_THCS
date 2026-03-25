90f3743cb693ff01d065a808c319848228e3d698
LeDucLoc-25174600220-19A4 <ldloc.25174600220@sv.uneti.edu.vn>
2026-03-25 17:10:06 +0700

    Delete 4.1_25174600228_NguyenQuangQuan directory

--- 4.1_25174600228_NguyenQuangQuan/bai4.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Nhap n: "))
-
-for i in range(n):
-    x = int(input("Nhap so: "))
-    if x < 0:
-        break
--- 4.1_25174600228_NguyenQuangQuan/bai9.py
+++
@@ -1,7 +0,0 @@
-while True:
-    user = input("Nhap ID: ")
-    pw = input("Nhap password: ")
-
-    if user == "admin" and pw == "123":
-        print("Dang nhap thanh cong!")
-        break
