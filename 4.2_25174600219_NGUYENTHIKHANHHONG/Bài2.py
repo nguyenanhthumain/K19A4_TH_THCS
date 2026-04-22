80b31b8dc727e86a9af6b6942a778fbb1801f49b
Nguyenthikhanhhong25174600219KHDL19A4HN <nguyenthikhanhhong06ss@gmail.com>
2026-04-22 10:30:18 +0700

    Delete 4.2_25174600219_NGUYENTHIKHANHHONG directory

--- 4.2_25174600219_NGUYENTHIKHANHHONG/Bài2.py
+++
@@ -1,8 +0,0 @@
-x = int(input("Nhập tử số: "))
-while True:
-    y = int(input("Nhập mẫu số: "))
-    if y != 0:
-        break
-    print("Mẫu số không được bằng 0. Hãy nhập lại!")
-
-print(f"Phân số: {x}/{y}")
